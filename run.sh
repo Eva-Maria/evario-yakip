d9f5e181e036179d984067b480292624288ba791
Mario Neises <git@mnio.net>
2016-06-26 11:03:13 +0200

    run script shebang

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 javac -cp "yakip.jar:lib:src" -d out src/Launcher.java
 
 java -d64 -Xmx6500m -Xms6500m -Xss2048k -Djava.library.path=lib/native -cp "yakip.jar:lib:out" Launcher $1 $2

abe7684d65039dbb76b1e59e942c038880f8b939
Mario Neises <git@mnio.net>
2016-07-03 13:29:42 +0200

    invert node list + more heat

--- src/Config.java
+++ src/Config.java
@@ -12,7 +12,7 @@ public class Config {
     // stones: 0 fast, 1 middle, 2 fast
     public static final int[] WEIGHT_EMPTY_FIELD = new int[]{1, 3, 3};
     public static final int[] WEIGHT_SELF_COLORED_FIELD = new int[]{5, 5, 5}; //+HEAT
-    public static final int[] WEIGHT_OPPONENT_COLORED_FIELD = new int[]{3, 1, 1};
+    public static final int[] WEIGHT_OPPONENT_COLORED_FIELD = new int[]{3, 1, 1}; //+HEAT
 
     public static final String HOSTNAME = "127.0.0.1";
     public static final String[] SERVER_ARGS = {"800", "600", "60", SEED + ""};
